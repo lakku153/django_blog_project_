c24052d42b7d966524228f375f47062880630763
Lokesh Kumar Saini <lokesh726888@gmail.com>
2025-04-24 14:56:56 +0530

    user profile and login logout

--- django__project/settings.py
+++ django__project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -128,3 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap4"]
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+LOGIN_REDIRECT_URL='blog-home'
+
+LOGIN_URL='login'
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -9,12 +11,19 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request,f'Your account has been created.')
+            return redirect('login')
     else:
         form=UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
 
+def custom_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html')
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
 
 
 # types of messages
